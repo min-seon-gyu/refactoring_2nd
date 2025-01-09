6469b279fb9cb1b75be2cf87ca3660a42bfb0ba8
Andrew Min <gcael@naver.com>
2025-01-09 16:12:01 +0900

    chapter1 - 반복문 쪼개기(statement)

--- src/main/kotlin/chapter1/Statement.kt
+++ src/main/kotlin/chapter1/Statement.kt
@@ -57,9 +57,12 @@ fun statement(invoice: Invoice, plays: Plays): String {
     var result = "청구 내역 (고객명: ${invoice.customer})\n"
 
     for (perf in invoice.performances) {
-        volumeCredits = volumeCreditsFor(perf)
         result += "  ${playFor(perf).name}: $${usd(amountFor(perf))} (${perf.audience}석)\n"
         totalAmount += amountFor(perf)
+    }
+
+    for (perf in invoice.performances) {
+        volumeCredits += volumeCreditsFor(perf)
     }
 
     result += "총액: $${usd(totalAmount)}\n"
